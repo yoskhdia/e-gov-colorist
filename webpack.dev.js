96c4fc56b9db0602bae969420f32ce18bb07e406
yoskhdia <yoskhdia@users.noreply.github.com>
2022-10-08 20:20:34 +0900

    fox eslint errors

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+/* eslint-enable */
 
 module.exports = merge(common, {
     mode: "development",
