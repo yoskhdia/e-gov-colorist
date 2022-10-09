38d636a4256d2dda3b481458a640d2adb742a335
yoskhdia <yoskhdia@users.noreply.github.com>
2022-10-09 15:44:32 +0900

    Update dependencies & config to latest - update package.json   - remove awesome-typescript-loader - fix eslintrc.js   - remove `prettier/@typescript-eslint` - fix tailwind.config.js   - remove a purge option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./dist/popup.html"],
-    content: ["./src/**/*.{html,js,tsx}"],
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./dist/popup.html"],
     theme: {
         extend: {},
     },
