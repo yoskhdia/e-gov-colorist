131ee333ec255a3c6ba72cc3a6ebb30df638916e
Yoshitaka Okuda <yoskhdia@users.noreply.github.com>
2022-10-17 02:46:02 +0900

    Manifest V3 (#3)
    - firefox, chromeでmanifest.jsonを切り替えられるよう対応
    - rename backgroundPage.ts -> background.ts
    - background.tsをV3で動作するよう修正
      - addListenerで非同期処理を行う場合の返り値をtrueに修正 (https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage)

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,9 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 /* eslint-enable */
 
-module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-});
+module.exports = function (env) {
+    return merge(common(env), {
+        mode: "development",
+        devtool: "inline-source-map",
+    });
+};
